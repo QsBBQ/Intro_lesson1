7ef8c7ecb1d94a821dbda8f614c89ab867d1272e
Quinn Avila <hkdnow@yahoo.com>
2014-03-01 09:29:45 -0600

    Ok revamping

--- bj21.rb
+++ bj21.rb
@@ -10,18 +10,25 @@ name = gets.chomp
 ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 deck = []
-suits.each do |s|
-  ranks.each do |r|
-    deck.push([r, s])
+
+#Building Deck 
+  suits.each do |s|
+    ranks.each do |r|
+      deck.push([r, s])
+    end
   end
-end
+
+#Shuffling Deck
 shuffled = deck.shuffle
+
+#Trying to deal
+#need to turn these into methods.
 count = 0
 deckcount = 0
 p_hand = []
 d_hand = []
-while deckcount < 26
-  deckcount = 1 + deckcount
+#while deckcount < 26
+  #deckcount = 1 + deckcount
   while count < 2
     count = 1 + count
   
@@ -30,13 +37,18 @@ while deckcount < 26
     shuffled.delete_at(0)
     shuffled.delete_at(1)
   end
-  if p_hand[0][0].to_i + p_hand[1][0].to_i < 21
-    puts "21!"
-  else
-    break
+  if p_hand[0][0].to_i + p_hand[1][0].to_i == 21
+    puts "Player 21!"
+    
+  elsif d_hand[0][0].to_i + d_hand[1][0].to_i == 21
+    puts "Dealer 21!"
+    
+  elsif p_hand[0][0].to_i + p_hand[1][0].to_i < 21
+    puts "Player do you want to hit?"
+
      
    end
-end
+#end
 
 puts "Players hand #{p_hand}"
 puts "Dealers hand #{d_hand}"
