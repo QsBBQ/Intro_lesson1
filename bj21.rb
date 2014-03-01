d6594b39e4e3f610318bd5121d801746f8c1b7b0
Quinn Avila <hkdnow@yahoo.com>
2014-03-01 11:51:31 -0600

    Working after watching videos

--- bj21.rb
+++ bj21.rb
@@ -55,5 +55,82 @@ p1_total = calculate_total(p1_hand)
 puts "Players hand #{p1_hand[0]} and #{p1_hand[1]}, for a total of #{p1_total}"
 puts "Dealers hand #{d_hand[0]} and #{d_hand[1]}, for a total of #{d_total}"
 puts
-puts "1) hit 2) stay"
-hit_or_stay = gets.chomp
+
+#Player turn
+
+
+if p1_total == 21
+  puts "Congrates 21!"
+  exit
+end
+
+while p1_total < 21
+  puts "1) hit 2) stay"
+  hit_or_stay = gets.chomp
+  if !['1', '2'].include?(hit_or_stay)
+    puts "Error you must enter 1 or 2"
+    next
+  end
+
+  if hit_or_stay == "2"
+    puts "Player staying"
+    break
+  end
+  #hit
+  new_card = deck.pop
+  puts "Dealing card to player: #{new_card}"
+  p1_hand << new_card
+  p1_total = calculate_total(p1_hand)
+  puts "Player P1 total #{p1_total}"
+  if p1_total == 21
+    puts "Congrates 21!"
+    exit
+  elsif p1_total > 21
+    puts "BUSTED!"
+    exit
+  end
+end
+
+#Dealer turn
+
+if d_total == 21
+  puts "Dealer wins 21!"
+  exit
+end
+
+while d_total < 17
+  #hit
+  new_card = deck.pop
+  puts "Dealing new card for dealer: #{new_card}"
+  d_hand << new_card
+  d_total = calculate_total(d_hand)
+  puts "Dealer total now: #{d_total}"
+  if d_total == 21
+    puts "Dealer wins! You loss!"
+    exit
+  elsif d_total > 21
+    puts "Dealer busts! You win!"
+    exit
+  end
+end
+
+#Compare hand
+puts "Dealer's cards: "
+d_hand.each do |card|
+puts "=> #{card}"
+end
+puts
+
+puts "Your cards:"
+p1_hand.each do |card|
+  puts "=> #{card}"
+end
+puts
+
+if d_total > p1_total
+  puts "Sorry dealer won"
+elsif d_total < p1_total
+  puts "You win!"
+else
+  puts "House always wins!"
+end
