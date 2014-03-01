a1c76cebabea3fd1828eee527f07778bf2b3828b
Quinn Avila <hkdnow@yahoo.com>
2014-03-01 11:21:43 -0600

    almost working

--- bj21.rb
+++ bj21.rb
@@ -1,64 +1,59 @@
 #!/usr/bin/ruby env
-#Lesson1 Blackjack game
+#Lesson1 Blackjack game procedural
 
-puts "Welcome to QsBJ21"
-
-puts "Time to play Blackjack"
-puts "What is you name?"
-name = gets.chomp
-
-ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
-suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-deck = []
-
-#Building Deck probably need method
-def build_deck
-  suits.each do |s|
-    ranks.each do |r|
-      deck.push([r, s])
+def calculate_total(cards)
+  #Build the values and calculate total
+  arr = cards.map{ |e| e[1] }
+  
+  total = 0
+  arr.each do |value|
+    if value == "A"
+      total += 11
+    elsif value.to_i == 0
+      total = total += 10
+    else
+      total += value.to_i
     end
   end
+
+  #correct for aces
+  arr.select{|e| e == "A"}.count.times do
+    total -= 10 if total > 21
+  end
+  return total
 end
+
+
+puts "Welcome to QsBJ21"
+puts "Time to play Blackjack"
+
+puts "What is your name?"
+name = gets.chomp
+
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+#Building Deck
+deck = suits.product(ranks)
 
 #Shuffling Deck
-shuffled = []
-def myshuffle
-  shuffled = deck.shuffle
-end
-#Trying to deal
-#need to turn these into methods.
+deck.shuffle!
 
-def deal()
-end
-
-count = 0
-deckcount = 0
-p_hand = []
+#Dealer
+p1_hand = []
 d_hand = []
-#while deckcount < 26
-  #deckcount = 1 + deckcount
-  while count < 2
-    count = 1 + count
-  
-    p_hand.push(shuffled[0])
-    d_hand.push(shuffled[1])
-    shuffled.delete_at(0)
-    shuffled.delete_at(1)
-  end
 
+p1_hand << deck.pop
+d_hand << deck.pop
+p1_hand << deck.pop
+d_hand << deck.pop
 
-  if p_hand[0][0].to_i + p_hand[1][0].to_i == 21
-    puts "Player 21!"
-    
-  elsif d_hand[0][0].to_i + d_hand[1][0].to_i == 21
-    puts "Dealer 21!"
-    
-  elsif p_hand[0][0].to_i + p_hand[1][0].to_i < 21
-    puts "Player do you want to hit?"
+d_total = calculate_total(d_hand)
+p1_total = calculate_total(p1_hand)
 
-     
-   end
-#end
-
-#puts "Players hand #{p_hand}"
-#puts "Dealers hand #{d_hand}"
+#Show Cards
+puts "Players hand #{p1_hand[0]} and #{p1_hand[1]}, for a total of #{p1_total}"
+puts "Dealers hand #{d_hand[0]} and #{d_hand[1]}, for a total of #{d_total}"
+puts
+puts "1) hit 2) stay"
+hit_or_stay = gets.chomp
